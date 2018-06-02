c70d5f9cb75dc017f5f8b8f22fa9cb08d812762e
Murilo Andrade <muriloandradealmeida@gmail.com>
2018-06-02 19:38:10 -0300

    Contagem de comparações adicionada

--- HeapSort.java
+++ HeapSort.java
@@ -1,12 +1,14 @@
 //Exemplo de contagem em Python: https://ideone.com/Te5Wul
-//Faltando a contagem das comparações. Onde colocar os comp++?
+//A contagem das comparações está correta? Se fosse para colocar uma variável comp++,
+//em quais partes seria necessário colocá-las?
 
 package heapsort;
 
 public class HeapSort {
-    //public int comp=0;
+    public int comp=0;
     
-    public void ordenarArvore(int[] vetor, int tamanho, int raiz){
+    public int ordenarArvore(int[] vetor, int tamanho, int raiz){
+        comp = 0;
         int maior = raiz;
         int indiceEsquerda = 2*raiz + 1;
         int indiceDireita = 2*raiz + 2;
@@ -21,21 +23,25 @@ public class HeapSort {
         
         //Se o maior não for a raiz
         if(maior != raiz){
+            comp+=1;
             int troca = vetor[raiz];
             vetor[raiz] = vetor[maior];
             vetor[maior] = troca;
             
-            ordenarArvore(vetor, tamanho, maior);
+            comp += ordenarArvore(vetor, tamanho, maior);
         }
+        return comp;
     }
     
     public void ordenarVetor(int[] vetor){
         int tamanho = vetor.length;
+        comp = 0;
         
         //Constrói a arvore (reorganiza o vetor)
-        for (int i = (tamanho/2 -1); i >= 0; i--)
+        for (int i = (tamanho/2 -1); i >= 0; i--){
             ordenarArvore(vetor, tamanho, i);
-        
+            comp += ordenarArvore(vetor, tamanho, i);
+        }
         //Extrair elemento um a um da heap
         for (int i= (tamanho-1); i >= 0; i--){
             //Mover a raiz atual para o fim
@@ -43,7 +49,7 @@ public class HeapSort {
             vetor[0] = vetor[i];
             vetor[i] = temp;
             
-            ordenarArvore(vetor, i, 0);
+            comp += ordenarArvore(vetor, i, 0);
         }
     }
     
